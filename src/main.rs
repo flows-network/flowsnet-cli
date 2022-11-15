7fa673f3830fa65e0a9926842c73ba7641e6a495
DarumaDocker <DarumaDocker@gmail.com>
2022-11-15 18:27:03 +0800

    run proxy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,48 @@
-use std::net::TcpListener;
+use anyhow::Result;
+use std::path::PathBuf;
+use tokio::{signal, sync::broadcast};
+use tracing_subscriber::EnvFilter;
 
-fn main() {
-    if let Some(available_port) = get_available_port() {
-        println!("port `{}` is available", available_port);
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (shutdown_tx, shutdown_rx) = broadcast::channel::<bool>(1);
+    tokio::spawn(async move {
+        if let Err(e) = signal::ctrl_c().await {
+            // Something really weird happened. So just panic
+            panic!("Failed to listen for the ctrl-c signal: {:?}", e);
+        }
+
+        if let Err(e) = shutdown_tx.send(true) {
+            // shutdown signal must be catched and handle properly
+            // `rx` must not be dropped
+            panic!("Failed to send shutdown signal: {:?}", e);
+        }
+    });
+
+    {
+        let is_atty = atty::is(atty::Stream::Stdout);
+
+        let level = "info"; // if RUST_LOG not present, use `info` level
+        tracing_subscriber::fmt()
+            .with_env_filter(
+                EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::from(level)),
+            )
+            .with_ansi(is_atty)
+            .init();
     }
-}
 
-fn get_available_port() -> Option<u16> {
-    (8000..9000).find(|port| port_is_available(*port))
-}
+    tokio::spawn(async {
+        let args = rathole::Cli {
+            config_path: Some(PathBuf::from("./target/debug/client.toml")),
+            server: false,
+            client: true,
+            genkey: None,
+        };
 
-fn port_is_available(port: u16) -> bool {
-    match TcpListener::bind(("127.0.0.1", port)) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
+        rathole::run(args, shutdown_rx).await
+    });
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    Ok(())
 }
